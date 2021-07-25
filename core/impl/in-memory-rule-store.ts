80deff55333b1d51be480318249aefc7271aa498
fransua <andrey-ne@mail.ru>
2021-07-25 15:54:03 +0300

    add inmemory impl

--- core/impl/in-memory-rule-store.ts
+++ core/impl/in-memory-rule-store.ts
@@ -1,15 +1,20 @@
 import {AccessInheritanceRule, IRuleStore, URI} from "../contracts";
 
-export class InMemoryRuleStore implements IRuleStore{
+export class InMemoryRuleStore implements IRuleStore {
 
     protected rules: {
         [key: string]: AccessInheritanceRule[]
-    } = {
-
-    };
+    } = {};
 
     public async GetRules(resource: URI): Promise<AccessInheritanceRule[]> {
         return this.rules[resource] ?? [];
+    }
+
+    public async UpdateRules(resource: URI, add: AccessInheritanceRule[], remove: AccessInheritanceRule[]): Promise<void> {
+        this.rules[resource] = [
+            ...(this.rules[resource] ?? []),
+            ...add
+        ].filter(x => !remove.map(x => x.InheritedFrom).includes(x.InheritedFrom));
     }
 
 }
